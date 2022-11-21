3536fb6b4736ef9547e23ccf574ddc89450b4621
joaobritonascimento <joaobritonascimento@gmail.com>
2022-11-21 19:49:23 -0300

    Atualizando usuarioController.java

--- src/main/java/org/senai/prjjava/UsuarioController.java
+++ src/main/java/org/senai/prjjava/UsuarioController.java
@@ -1,12 +1,18 @@
 package org.senai.prjjava;
 
+import java.util.Optional;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -20,19 +26,31 @@ public class UsuarioController {
     @Autowired
     UsuarioRepository uRepository;
 
-    @PostMapping("/add")
+    @PostMapping("/")
+    public @ResponseBody Integer addUsuario(@RequestBody Usuario objU) {
+        uRepository.save(objU);
+        return objU.getId();
+    }
 
-    public @ResponseBody String addUsuario(
-        @RequestParam String nome, 
-        @RequestParam String email){
-            Usuario objU = new Usuario();
-                    objU.setNome(nome);
-                    objU.setEmail(email);
-                    uRepository.save(objU);
-                    return "Salvo";  
-        }
-    @GetMapping ("/all")
-    public @ResponseBody Iterable<Usuario> buscarUsuarios(){
+    @GetMapping("/")
+    public @ResponseBody Iterable<Usuario> buscarUsuarios() {
         return uRepository.findAll();
-    }    
-}
+    }
+    
+    // http://localhost:8080/api/usuario/2 - nesse exemlo é usado uma variável de path(caminho)
+    @GetMapping("/{id}")
+    public @ResponseBody Optional<Usuario> buscarUsuario(@PathVariable Integer id){
+        return uRepository.findById(id);
+    }
+
+    @PutMapping("/")
+    public @ResponseBody Usuario atualizar(@RequestBody Usuario objU){
+        uRepository.save(objU);
+        return objU;
+    }
+
+    @DeleteMapping("/{id}")
+    public @ResponseBody String apagar(@PathVariable Integer id){
+        uRepository.deleteById(id);
+        return "Ok ao apagar!";
+    }
