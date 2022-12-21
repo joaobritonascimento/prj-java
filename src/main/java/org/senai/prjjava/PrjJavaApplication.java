a431d6427f86c816f19cd9d678e1bb6e8dff5d5c
joaobritonascimento <joaobritonascimento@gmail.com>
2022-12-21 01:19:13 +0000

    atualizando

--- src/main/java/org/senai/prjjava/PrjJavaApplication.java
+++ src/main/java/org/senai/prjjava/PrjJavaApplication.java
@@ -6,19 +6,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication //Os arrobas significam INJECT
-//Foi necessario digitar "alt+shift+o" para importar a biblioteca import org.springframework.web.bind.annotation.RestController
-@RestController 
+@SpringBootApplication
+@RestController
 public class PrjJavaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrjJavaApplication.class, args);
 	}
 
-	@GetMapping("/hello")  //
-	//Se o "name" estiver vazio a pagina voltará a palavra padrão "world"
+	@GetMapping("/hello")
 	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
-	return String.format("Hello %s!", name);
+		return String.format("Hello %s!", name);
 	}
 
 }
